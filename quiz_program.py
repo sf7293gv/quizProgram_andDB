7b5eb34b00961b027988f7105501b40d3f133c0c
Sonic <sf7293gv@go.minnstate.edu>
2021-10-07 16:48:25 -0500

    Added a method to quiz the user

--- quiz_program.py
+++ quiz_program.py
@@ -36,21 +36,54 @@ def topic_user_choice(topics_list):
 """ This method will get the question and answers from the db and store them in a dictionary"""
 def get_questions_answers(topic):
     qandaDict = {}
+    counter = 0
     with sqlite3.connect(db) as conn:
         query = conn.execute('select * from questionTable where category = ?', (topic,))
-        counter = 0
         for r in query:
             question = (r[1])
             answerList = (r[2], r[3], r[4], r[5])
             qandaDict[question] = answerList
             counter = counter + 1
     conn.close()
-    return qandaDict
+    return (qandaDict, counter)
 
+""" This method will show the user the amount of questions for a topic, 
+will ask how many questions user wants to answer, 
+will list the answer list for each question and will store/compare the user answer """
+def quiz_user(qandaDict, amountQuestions): 
+    nq = int(input(f'Topic has {amountQuestions} questions. How many do you want to answer? '))
+    c = 0
+    answers = []
+    for item in qandaDict.items():
+        if c < nq:
+            print(f'Question: {item[0]}')
+            print('choose one of these answers for the question: ')
+            for i in range (0, len(item[1])):
+                print(f'{i+1}- {item[1][i]}')
+            user_input = int(input('Choose the number of the answer: '))
+            user_answer = item[1][user_input - 1]
+            correct_answer = item[1][0]
+            answers = (user_answer, correct_answer)
+            # print(f'Your answer is : {user_answer}')
+            # if (user_answer == correct_answer):
+            #     print('Correct!!')
+            # else:
+            #     print(f'Correct answer: {correct_answer}')
+
+            # print(item[1][0])
+            c = c + 1
+        else:
+            break
+        return answers
 
 list = get_quiz_topics()
 user_topic_choice = topic_user_choice(list)
-qNaDictionary = get_questions_answers(user_topic_choice)
+res = get_questions_answers(user_topic_choice)
+qNaDictionary = res[0]
+questions_amount = res[1]
+
+answers_list = quiz_user(qNaDictionary, questions_amount)
+
 
 
 # topics_list.append(firstRow[0])
@@ -64,31 +97,7 @@ qNaDictionary = get_questions_answers(user_topic_choice)
 
 
 
-# query = db.execute(f'select * from questionTable where category = "{categoryChoice}"')
 
 
 
 
-# nq = int(input(f'{categoryChoice} has {counter} questions. How many do you want to answer? '))
-
-# c = 0
-
-# for item in qandaDict.items():
-#     if c < nq:
-#         print(f'Question: {item[0]}')
-#         print('choose one of these answers for the question: ')
-#         for i in range (0, len(item[1])):
-#             print(f'{i+1}- {item[1][i]}')
-#         user_input = int(input('Choose the number of the answer: '))
-#         user_answer = item[1][user_input - 1]
-#         correct_answer = item[1][0]
-#         print(f'Your answer is : {user_answer}')
-#         if (user_answer == correct_answer):
-#             print('Correct!!')
-#         else:
-#             print(f'Correct answer: {correct_answer}')
-
-#         # print(item[1][0])
-#         c = c + 1
-#     else:
-#         break

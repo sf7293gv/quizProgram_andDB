ec5c686ba7b4fabeabf8fc7a8f350579dd12c0fd
Sonic <sf7293gv@go.minnstate.edu>
2021-10-08 01:20:09 -0500

    Modified data types in quiz_results table (date from int to text)

--- quiz_testing.py
+++ quiz_testing.py
@@ -1,21 +1,32 @@
-import sqlite3
-from sqlite3.dbapi2 import connect
+# import sqlite3
+# from sqlite3.dbapi2 import connect
 
-db = 'quizDatabase.db'
+# db = 'quizDatabase.db'
 
-conn = sqlite3.connect(db)
+# conn = sqlite3.connect(db)
 
-result = conn.execute('SELECT * FROM questionTable')
-qandaDict = {}
-for r in result:
-    question = r[1]
-    category = r[6]
-    difficulty = r[7]
-    question_points = r[8]
-    qInfo_list = (question, category, difficulty, question_points)
-    answerList = (r[2], r[3], r[4], r[5])
-    qandaDict[qInfo_list] = answerList
-    # counter = counter + 1
+# result = conn.execute('SELECT * FROM questionTable')
+# qandaDict = {}
+# for r in result:
+#     question = r[1]
+#     category = r[6]
+#     difficulty = r[7]
+#     question_points = r[8]
+#     qInfo_list = (question, category, difficulty, question_points)
+#     answerList = (r[2], r[3], r[4], r[5])
+#     qandaDict[qInfo_list] = answerList
+#     # counter = counter + 1
 
-for item in qandaDict.items():
-    print(item)
+# for item in qandaDict.items():
+#     print(item)
+
+import time
+
+
+Time = time.time()
+
+print(Time)
+
+
+
+print(time.asctime())

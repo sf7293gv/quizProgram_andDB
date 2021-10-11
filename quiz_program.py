7137ef56ceb19e675e73fcacd30c255c0f19e8dd
Sonic <sf7293gv@go.minnstate.edu>
2021-10-11 00:52:01 -0500

    Modified some methods and added a new one to make the program show (time taken/ amount qs answered)

--- quiz_program.py
+++ quiz_program.py
@@ -2,7 +2,6 @@ import sqlite3
 import time
 from sqlite3.dbapi2 import connect
 
-
 db = 'quizDatabase.db'
 
 """ This method will get all the topics/categories available in the table """
@@ -60,27 +59,34 @@ def get_questions_answers(topic):
     return (qandaDict, counter)
 
 """ This method will show the user the amount of questions for a topic, 
-will ask how many questions user wants to answer and list questions/info, 
-will list the answer list for each question and will store/compare the user answer """
-def quiz_user(qandaDict, amountQuestions): 
+    will ask how many questions user wants to answer """
+def amount_of_questions_to_ask(amountQuestions):
     while (True):
         nq = int(input(f'Topic has {amountQuestions} questions. How many do you want to answer? '))
         if nq > 0 and nq <= amountQuestions:
             break
+    return nq
 
+
+""" This method  and list questions/info, 
+will list the answer list for each question and will store/compare the user answer """
+def quiz_user(qandaDict, user_questions_amount):     
     c = 0
     answers = {}
     for item in qandaDict.items():
-        if c < nq:
+        if c < user_questions_amount:
             question = item[0][0]
             category = item[0][1]
             difficulty = item[0][2]
             q_points = item[0][3]
             q_id = item[0][4]
+
             print(f'Question: {question}, category: {category}, difficulty: {difficulty}, points available: {q_points}, question id: {q_id}')
             print('choose one of these answers for the question: ')
+
             for i in range (0, len(item[1])):
                 print(f'{i+1}- {item[1][i]}')
+
             while (True):
                 user_input = int(input('Choose the number of the answer: '))
                 if user_input > 0 and user_input <= len(item[1]):                    
@@ -89,8 +95,7 @@ def quiz_user(qandaDict, amountQuestions):
             user_answer = item[1][user_input - 1]
             correct_answer = item[1][0]
             q_data = (question, category, difficulty, q_points, q_id)
-            answers[q_data] = (user_answer,correct_answer)
-            
+            answers[q_data] = (user_answer,correct_answer)            
             c = c + 1
         else:
             break
@@ -99,24 +104,18 @@ def quiz_user(qandaDict, amountQuestions):
 
 """ This method will be passed a dictionary that contains 
     (questions, user answers, correct answers) and will compare between the answers """
-def compare_answers(answers_dict):
+def compare_answers(answers_dict):   
     for key, value in answers_dict.items():
         question = key
         user_answer = value[0]
-        correct_answer = value[1]
-        print('key val pair:')
-        print(key, value)
-        
-        print(f'For the question: {question} User answer: {user_answer}, Correct answer: {correct_answer}')
+        correct_answer = value[1]     
+        print(f'For the question: {question[0]} User answer is: {user_answer}')
         if (user_answer == correct_answer):
             print('Correct!!')
         else:
             print(f'Correct answer: {correct_answer}')
 
 
-# insert INTO quiz_results (userID, qID, timeStarted, timeEnded, question, answer, correct, questionPoints, pointsEarned)VALUES (
-# 	"one", 1, 10, 11, 1, 2, 1, 20, 15
-# );
 
 """ This method will be passed two dictionaries, first one contains questions and user answers,
     second contains more data for each question 
@@ -146,7 +145,7 @@ def add_info_quiz_results_table(user_answers_dict):
                 list = (user_name, qID, timestarted, timeended, q, uA, correct, questionPoints, pointsEarned)
                 print(list)
                 conn.execute(query, list)
-                print(item)                
+                # print(item)                
         except sqlite3.Error:
             print('Error inserting data to table')
 
@@ -158,13 +157,25 @@ def get_user_id():
         if (userID != ""):
             break
     return userID
-        
+
+def get_quiz_results(questions_amount):
+    time_taken = quiz_end_time - quiz_start_time
+    time_taken = '%.2f' % time_taken
+    print(f'It took you {time_taken} seconds to finish the quiz.')
+    print(f'Amount of questions you answered: {questions_amount}')
+
 
 list = get_quiz_topics()
 user_topic_choice = topic_user_choice(list)
 res = get_questions_answers(user_topic_choice)
 question_dictionary = res[0]
 questions_amount = res[1]
-user_answers_dict = quiz_user(question_dictionary, questions_amount)
+quiz_start_time = time.time()
+user_questions_amount = amount_of_questions_to_ask(questions_amount)
+user_answers_dict = quiz_user(question_dictionary, user_questions_amount)
 compare_answers(user_answers_dict)
+quiz_end_time = time.time()
 add_info_quiz_results_table(user_answers_dict)
+get_quiz_results(user_questions_amount)
+
+
